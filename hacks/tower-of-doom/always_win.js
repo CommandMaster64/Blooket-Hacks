5e821d27ccf643771da5ea97243f96ffe908b82f
CommandMaster64 <81648386+CommandMaster64@users.noreply.github.com>
2023-02-10 13:29:40 -0800

    Create always_win.js

---
+++ hacks/tower-of-doom/always_win.js
@@ -0,0 +1,11 @@
+function set() {
+    if (!!Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.enemyCard) {
+        Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.enemyCard.strength = 0;
+        Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.enemyCard.charisma = 0;
+        Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.enemyCard.wisdom = 0;
+        Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.myCard.strength = 999;
+        Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.myCard.charisma = 999;
+        Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.myCard.wisdom = 999;
+    }
+}
+setInterval(set, 1);
