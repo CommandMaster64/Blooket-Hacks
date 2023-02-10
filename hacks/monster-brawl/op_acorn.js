5ce371711c591a62da2642aba3344117266fc4df
CommandMaster64 <81648386+CommandMaster64@users.noreply.github.com>
2023-02-10 10:19:57 -0700

    Update op_acorn.js

--- hacks/monster-brawl/op_acorn.js
+++ hacks/monster-brawl/op_acorn.js
@@ -4,7 +4,7 @@ Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.
 Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[1].data.dmg = 999;
 Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[1].data.speed = 999;
 Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[1].data.maxTargets = 999;
-Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[1].data.lifespan = 99999;
+Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[1].data.lifespan = 2000;
 Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[1].data.fireRate = 999;
 Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[1].data.intervalRate = 1;
 Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[1].data.knockback = 999;
