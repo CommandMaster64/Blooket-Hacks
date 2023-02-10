4a5620a85dfe10c656131b8f8685b1e20ed22a3e
CommandMaster64 <81648386+CommandMaster64@users.noreply.github.com>
2023-02-10 12:51:50 -0800

    Update and rename set_orders_to_none.js to remove_customers.js

--- hacks/cafe/remove_customers.js
+++ hacks/cafe/remove_customers.js
@@ -1,10 +1,10 @@
-function toast() {
+function removeCustomers() {
     if (!!Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.customers) {
         for (let i = 0; i < Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.customers.length; i++) {
             if (!!Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.customers[i].order) {
-                Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.customers[i].order.Toast = 0;
+                Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.removeCustomer(i, 1);
             }
         }
     }
 }
-setInterval(toast, 1);
+setInterval(removeCustomers, 1);
