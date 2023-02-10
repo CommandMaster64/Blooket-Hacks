ed242a81735f8abdc9a4475215ac1fe1c6548111
CommandMaster64 <81648386+CommandMaster64@users.noreply.github.com>
2023-02-10 14:25:53 -0800

    Create best_catch.js

---
+++ hacks/fishing-frenzy/best_catch.js
@@ -0,0 +1,1 @@
+Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.fish = {isSpecial: false, name: "Frog", rarity: "Hacks", tier: "Hacks", weight: 99999};
