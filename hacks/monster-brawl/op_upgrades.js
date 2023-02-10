7052eaf79a403fd6f0906ada80944054bb55e7bb
CommandMaster64 <81648386+CommandMaster64@users.noreply.github.com>
2023-02-10 14:11:48 -0800

    Update op_upgrades.js

--- hacks/monster-brawl/op_upgrades.js
+++ hacks/monster-brawl/op_upgrades.js
@@ -33,4 +33,14 @@ for (let i = 0; i < Object.values(document.querySelector("#app > div > div"))[1]
         Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].data.maxTargets = 999;
         Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].rate = 0.1;
     }
+    if (Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].name == "bubble") {
+        Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].data.dmg = 999;
+        Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].data.speed = 999;
+        Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].data.knockback = 999;
+        Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].data.fireRate = 1;
+        Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].data.intervalRate = 1;
+        Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].data.numProjectiles = 99;
+        Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].data.maxTargets = 999;
+        Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].rate = 0.1;
+    }
 }
