e201903feab10e4224a66bf3f6df42c0bc052690
CommandMaster64 <81648386+CommandMaster64@users.noreply.github.com>
2023-02-10 08:50:47 -0700

    Create auto-answer.js

---
+++ hacks/all-games/auto-answer.js
@@ -0,0 +1,10 @@
+function answer() {
+    if (!!document.getElementsByClassName("styles__answerContainer___3WS-k-camelCase")[0]) {
+        for (let i = 0; i < document.getElementsByClassName("styles__answerContainer___3WS-k-camelCase").length; i++) {
+            if (document.getElementsByClassName("styles__answerContainer___3WS-k-camelCase")[i].innerText == Object.values(document.querySelector("#app > div > div"))[1].children[1]._owner.stateNode.state.question.correctAnswers[0]) {
+                document.getElementsByClassName("styles__answerContainer___3WS-k-camelCase")[i].click();
+            }
+        }
+    }
+}
+setInterval(answer, 1);
