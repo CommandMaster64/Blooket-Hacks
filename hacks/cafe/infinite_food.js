0f0702057faf2ade551019e384f962825e5b36e0
CommandMaster64 <81648386+CommandMaster64@users.noreply.github.com>
2023-02-10 12:55:51 -0700

    Update infinite_food.js

--- hacks/cafe/infinite_food.js
+++ hacks/cafe/infinite_food.js
@@ -1,6 +1,8 @@
 function restock() {
-    for (let i = 0; i < Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.foods.length; i++) {
-        Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.foods[i].stock = 99;
+    if (!!Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.foods[0]) {
+        for (let i = 0; i < Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.foods.length; i++) {
+            Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.foods[i].stock = 99;
+        }
     }
 }
 setInterval(restock, 1);
