3b47f304f771740414b3f398b1b9edc5b7cca171
CommandMaster64 <81648386+CommandMaster64@users.noreply.github.com>
2023-02-10 10:51:26 -0700

    Create op_upgrades.js

---
+++ hacks/monster-brawl/op_upgrades.js
@@ -0,0 +1,17 @@
+for (let i = 0; i < Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions.length; i++) {
+    if (Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].name == "darkEnergy") {
+        Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].data.dmg = 999;
+        Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].data.speed = 999;
+        Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].data.knockback = 999;
+        Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].rate = 0.1;
+    }
+    if (Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].name == "nut") {
+        Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].data.dmg = 999;
+        Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].data.speed = 999;
+        Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].data.knockback = 999;
+        Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].data.fireRate = 1;
+        Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].data.intervalRate = 1;
+        Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].data.numProjectiles = 99;
+        Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].rate = 0.1;
+    }
+}
