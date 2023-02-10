f82c2d46734f02b254c3c8e0905311abd479b464
CommandMaster64 <81648386+CommandMaster64@users.noreply.github.com>
2023-02-10 09:40:08 -0700

    Create op_acorn.js

---
+++ hacks/monster-brawl/op_acorn.js
@@ -0,0 +1,11 @@
+Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[1].name = 'nut';
+Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[1].rate = 1;
+Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[1].title = "Crazy Acorns";
+Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[1].data.dmg = 999;
+Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[1].data.speed = 999;
+Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[1].data.maxTargets = 999;
+Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[1].data.lifespan = 99999;
+Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[1].data.fireRate = 1;
+Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[1].data.intervalRate = 1;
+Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[1].data.knockback = 999;
+Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[1].data.numProjectiles = 999;
