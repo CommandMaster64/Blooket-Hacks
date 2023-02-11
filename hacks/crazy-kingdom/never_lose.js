7c2c67b592010833f972c781b13e5936f1c5a3bc
CommandMaster64 <81648386+CommandMaster64@users.noreply.github.com>
2023-02-11 15:15:20 -0800

    Create never_lose.js

---
+++ hacks/crazy-kingdom/never_lose.js
@@ -0,0 +1,7 @@
+function setThings() {
+    Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.happiness = 999;
+    Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.materials = 999;
+    Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.people = 999;
+    Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.gold = 999;
+}
+setInterval(setThings, 1);
