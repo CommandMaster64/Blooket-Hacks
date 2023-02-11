b58c55e22a3316c543a5e63d2543e7618a546f5a
CommandMaster64 <81648386+CommandMaster64@users.noreply.github.com>
2023-02-11 15:40:19 -0800

    Create password_hacker.js

---
+++ hacks/crypto-hack/password_hacker.js
@@ -0,0 +1,5 @@
+for (let i = 0; i < document.getElementsByClassName("styles__button___2OOoS-camelCase").length; i++) {
+    if (document.getElementsByClassName("styles__button___2OOoS-camelCase")[i].innerText == Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.correctPassword) {
+        document.getElementsByClassName("styles__button___2OOoS-camelCase")[i].click();
+    }
+}
