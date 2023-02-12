71a47838aefac7685f43f765f521d39db7a0c8e7
CommandMaster64 <81648386+CommandMaster64@users.noreply.github.com>
2023-02-12 07:14:38 -0800

    Update set_gold.js

--- hacks/gold-quest/set_gold.js
+++ hacks/gold-quest/set_gold.js
@@ -1,4 +1,4 @@
-const set_gold (gold) => {
+const set_gold = (gold) => {
     Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.gold = gold;
     Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.gold2 = gold;
 };
