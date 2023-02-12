95ce5fa6ecf87f8b523f19efe318fb7231bb6fc2
CommandMaster64 <81648386+CommandMaster64@users.noreply.github.com>
2023-02-12 07:05:39 -0800

    Create chest_cheat.js

---
+++ hacks/gold-quest/chest_cheat.js
@@ -0,0 +1,14 @@
+function set_chest_cheat() {
+    if (!!document.getElementsByClassName("styles__choice1___27uim-camelCase")[0]) {
+        if (!document.getElementsByClassName("styles__choice1___27uim-camelCase")[0].innerHTML.includes(Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.choices[0].text)) {
+            document.getElementsByClassName("styles__choice1___27uim-camelCase")[0].innerHTML += Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.choices[0].text;
+        }
+        if (!document.getElementsByClassName("styles__choice2___1aP2D-camelCase")[0].innerHTML.includes(Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.choices[1].text)) {
+            document.getElementsByClassName("styles__choice2___1aP2D-camelCase")[0].innerHTML += Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.choices[1].text;
+        }
+        if (!document.getElementsByClassName("styles__choice3___2L6Q--camelCase")[0].innerHTML.includes(Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.choices[2].text)) {
+            document.getElementsByClassName("styles__choice3___2L6Q--camelCase")[0].innerHTML += Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.choices[2].text;
+        }
+    }
+}
+setInterval(set_chest_cheat, 1);
