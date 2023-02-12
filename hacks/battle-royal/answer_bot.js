0b87cf234ad6c0b26835ab0af22a4388fcf76a47
CommandMaster64 <81648386+CommandMaster64@users.noreply.github.com>
2023-02-11 18:34:27 -0800

    Create answer_bot.js

---
+++ hacks/battle-royal/answer_bot.js
@@ -0,0 +1,13 @@
+function answer() {
+    if (!!document.getElementsByClassName("styles__answerContainer___3WS-k-camelCase")[0]) {
+        for (let i = 0; i < document.getElementsByClassName("styles__answerContainer___3WS-k-camelCase").length; i++) {
+            if (document.getElementsByClassName("styles__answerContainer___3WS-k-camelCase")[i].innerText == Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.props.client.question.correctAnswers[0]) {
+                document.getElementsByClassName("styles__answerContainer___3WS-k-camelCase")[i].click();
+            }
+        }
+    }
+    if (!!document.getElementsByClassName("arts__regularBody___1TM6E-camelCase styles__background___2GulD-camelCase")[0]) {
+        document.getElementsByClassName("arts__regularBody___1TM6E-camelCase styles__background___2GulD-camelCase")[0].click();
+    }
+}
+setInterval(answer, 1);
