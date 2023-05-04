016fa21268c3cc24de77e55bfa42257434965e7c
CommandMaster64 <81648386+CommandMaster64@users.noreply.github.com>
2023-05-04 10:48:01 -0600

    Create set_person_to_negitive.js

---
+++ hacks/gold-quest/set_person_to_negitive.js
@@ -0,0 +1,3 @@
+Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.gold = -5000;
+Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.choices[0].type = "swap";
+Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.choices[0].text = "swap";
