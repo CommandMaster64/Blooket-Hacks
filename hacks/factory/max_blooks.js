7949d6310217a159b0926b98f284356aa90565f6
CommandMaster64 <81648386+CommandMaster64@users.noreply.github.com>
2023-09-08 13:16:40 -0700

    Update max_blooks.js

--- hacks/factory/max_blooks.js
+++ hacks/factory/max_blooks.js
@@ -1,10 +1,10 @@
-for (let i = 0; i < Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.choices.length; i++) {
-    Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.choices[i].cash = [999999];
-    Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.choices[i].time = [0.01];
-    Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.choices[i].price = [];
+for (let i = 0; i < Object.values(document.querySelector("#aeffdabedffdb > div > div"))[1].children[0]._owner.stateNode.state.choices.length; i++) {
+    Object.values(document.querySelector("#aeffdabedffdb > div > div"))[1].children[0]._owner.stateNode.state.choices[i].cash = [999999];
+    Object.values(document.querySelector("#aeffdabedffdb > div > div"))[1].children[0]._owner.stateNode.state.choices[i].time = [0.01];
+    Object.values(document.querySelector("#aeffdabedffdb > div > div"))[1].children[0]._owner.stateNode.state.choices[i].price = [];
 }
-for (let i = 0; i < Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.blooks.length; i++) {
-    Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.blooks[i].cash = [999999];
-    Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.blooks[i].time = [0.01];
-    Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.blooks[i].price = [];
+for (let i = 0; i < Object.values(document.querySelector("#aeffdabedffdb > div > div"))[1].children[0]._owner.stateNode.state.blooks.length; i++) {
+    Object.values(document.querySelector("#aeffdabedffdb > div > div"))[1].children[0]._owner.stateNode.state.blooks[i].cash = [999999];
+    Object.values(document.querySelector("#aeffdabedffdb > div > div"))[1].children[0]._owner.stateNode.state.blooks[i].time = [0.01];
+    Object.values(document.querySelector("#aeffdabedffdb > div > div"))[1].children[0]._owner.stateNode.state.blooks[i].price = [];
 }
