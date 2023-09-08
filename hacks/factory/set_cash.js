47e57451dade3bf56d3d9506f6e07e44694fdaca
CommandMaster64 <81648386+CommandMaster64@users.noreply.github.com>
2023-09-08 13:16:48 -0700

    Update set_cash.js

--- hacks/factory/set_cash.js
+++ hacks/factory/set_cash.js
@@ -1,3 +1,3 @@
 const setCash = (cash) => {
-    Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.cash = cash;
+    Object.values(document.querySelector("#aeffdabedffdb > div > div"))[1].children[0]._owner.stateNode.state.cash = cash;
 };
