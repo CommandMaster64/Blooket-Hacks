c917a2eab4a8f862bf59fcc73f7eed641a67bc22
CommandMaster64 <81648386+CommandMaster64@users.noreply.github.com>
2023-09-08 13:17:50 -0700

    Update op_upgrades.js

--- hacks/monster-brawl/op_upgrades.js
+++ hacks/monster-brawl/op_upgrades.js
@@ -1,46 +1,46 @@
-for (let i = 0; i < Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions.length; i++) {
-    if (Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].name == "darkEnergy") {
-        Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].data.dmg = 999;
-        Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].data.speed = 999;
-        Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].data.knockback = 999;
-        Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].rate = 0.1;
+for (let i = 0; i < Object.values(document.querySelector("#aeffdabedffdb > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions.length; i++) {
+    if (Object.values(document.querySelector("#aeffdabedffdb > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].name == "darkEnergy") {
+        Object.values(document.querySelector("#aeffdabedffdb > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].data.dmg = 999;
+        Object.values(document.querySelector("#aeffdabedffdb > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].data.speed = 999;
+        Object.values(document.querySelector("#aeffdabedffdb > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].data.knockback = 999;
+        Object.values(document.querySelector("#aeffdabedffdb > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].rate = 0.1;
     }
-    if (Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].name == "nut") {
-        Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].data.dmg = 999;
-        Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].data.speed = 999;
-        Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].data.knockback = 999;
-        Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].data.fireRate = 1;
-        Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].data.intervalRate = 1;
-        Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].data.numProjectiles = 99;
-        Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].rate = 0.1;
+    if (Object.values(document.querySelector("#aeffdabedffdb > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].name == "nut") {
+        Object.values(document.querySelector("#aeffdabedffdb > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].data.dmg = 999;
+        Object.values(document.querySelector("#aeffdabedffdb > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].data.speed = 999;
+        Object.values(document.querySelector("#aeffdabedffdb > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].data.knockback = 999;
+        Object.values(document.querySelector("#aeffdabedffdb > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].data.fireRate = 1;
+        Object.values(document.querySelector("#aeffdabedffdb > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].data.intervalRate = 1;
+        Object.values(document.querySelector("#aeffdabedffdb > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].data.numProjectiles = 99;
+        Object.values(document.querySelector("#aeffdabedffdb > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].rate = 0.1;
     }
-    if (Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].name == "pizza") {
-        Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].data.dmg = 999;
-        Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].data.speed = 999;
-        Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].data.knockback = 999;
-        Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].data.fireRate = 1;
-        Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].data.intervalRate = 1;
-        Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].data.numProjectiles = 999;
-        Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].rate = 0.1;
+    if (Object.values(document.querySelector("#aeffdabedffdb > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].name == "pizza") {
+        Object.values(document.querySelector("#aeffdabedffdb > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].data.dmg = 999;
+        Object.values(document.querySelector("#aeffdabedffdb > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].data.speed = 999;
+        Object.values(document.querySelector("#aeffdabedffdb > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].data.knockback = 999;
+        Object.values(document.querySelector("#aeffdabedffdb > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].data.fireRate = 1;
+        Object.values(document.querySelector("#aeffdabedffdb > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].data.intervalRate = 1;
+        Object.values(document.querySelector("#aeffdabedffdb > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].data.numProjectiles = 999;
+        Object.values(document.querySelector("#aeffdabedffdb > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].rate = 0.1;
     }
-    if (Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].name == "horseshoe") {
-        Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].data.dmg = 999;
-        Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].data.speed = 999;
-        Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].data.knockback = 999;
-        Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].data.fireRate = 1;
-        Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].data.intervalRate = 1;
-        Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].data.numProjectiles = 99;
-        Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].data.maxTargets = 999;
-        Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].rate = 0.1;
+    if (Object.values(document.querySelector("#aeffdabedffdb > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].name == "horseshoe") {
+        Object.values(document.querySelector("#aeffdabedffdb > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].data.dmg = 999;
+        Object.values(document.querySelector("#aeffdabedffdb > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].data.speed = 999;
+        Object.values(document.querySelector("#aeffdabedffdb > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].data.knockback = 999;
+        Object.values(document.querySelector("#aeffdabedffdb > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].data.fireRate = 1;
+        Object.values(document.querySelector("#aeffdabedffdb > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].data.intervalRate = 1;
+        Object.values(document.querySelector("#aeffdabedffdb > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].data.numProjectiles = 99;
+        Object.values(document.querySelector("#aeffdabedffdb > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].data.maxTargets = 999;
+        Object.values(document.querySelector("#aeffdabedffdb > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].rate = 0.1;
     }
-    if (Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].name == "bubble") {
-        Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].data.dmg = 999;
-        Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].data.speed = 999;
-        Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].data.knockback = 999;
-        Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].data.fireRate = 1;
-        Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].data.intervalRate = 1;
-        Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].data.numProjectiles = 99;
-        Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].data.maxTargets = 999;
-        Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].rate = 0.1;
+    if (Object.values(document.querySelector("#aeffdabedffdb > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].name == "bubble") {
+        Object.values(document.querySelector("#aeffdabedffdb > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].data.dmg = 999;
+        Object.values(document.querySelector("#aeffdabedffdb > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].data.speed = 999;
+        Object.values(document.querySelector("#aeffdabedffdb > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].data.knockback = 999;
+        Object.values(document.querySelector("#aeffdabedffdb > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].data.fireRate = 1;
+        Object.values(document.querySelector("#aeffdabedffdb > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].data.intervalRate = 1;
+        Object.values(document.querySelector("#aeffdabedffdb > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].data.numProjectiles = 99;
+        Object.values(document.querySelector("#aeffdabedffdb > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].data.maxTargets = 999;
+        Object.values(document.querySelector("#aeffdabedffdb > div > div"))[1].children[0]._owner.stateNode.state.levelUpOptions[i].rate = 0.1;
     }
 }
