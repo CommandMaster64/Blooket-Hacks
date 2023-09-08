cf37c9c6797b09739a4800abb42fe651df0185fd
CommandMaster64 <81648386+CommandMaster64@users.noreply.github.com>
2023-09-08 13:15:33 -0700

    Update remove_customers.js

--- hacks/cafe/remove_customers.js
+++ hacks/cafe/remove_customers.js
@@ -1,9 +1,9 @@
 function removeCustomers() {
-    if (!!Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.customers) {
-        for (let i = 0; i < Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.customers.length; i++) {
-            if (!!Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.customers[i].order && !Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.customers[i].w) {
-                Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.removeCustomer(i, 1);
-                Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.customers[i].w = 1;
+    if (!!Object.values(document.querySelector("#aeffdabedffdb > div > div"))[1].children[0]._owner.stateNode.state.customers) {
+        for (let i = 0; i < Object.values(document.querySelector("#aeffdabedffdb > div > div"))[1].children[0]._owner.stateNode.state.customers.length; i++) {
+            if (!!Object.values(document.querySelector("#aeffdabedffdb > div > div"))[1].children[0]._owner.stateNode.state.customers[i].order && !Object.values(document.querySelector("#aeffdabedffdb > div > div"))[1].children[0]._owner.stateNode.state.customers[i].w) {
+                Object.values(document.querySelector("#aeffdabedffdb > div > div"))[1].children[0]._owner.stateNode.removeCustomer(i, 1);
+                Object.values(document.querySelector("#aeffdabedffdb > div > div"))[1].children[0]._owner.stateNode.state.customers[i].w = 1;
             }
         }
     }
