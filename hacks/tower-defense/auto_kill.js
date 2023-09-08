4bfbaf6a414304cf4f2b4facda37ac8e0876245a
CommandMaster64 <81648386+CommandMaster64@users.noreply.github.com>
2023-09-08 13:18:19 -0700

    Update auto_kill.js

--- hacks/tower-defense/auto_kill.js
+++ hacks/tower-defense/auto_kill.js
@@ -1,6 +1,6 @@
 function killAll() {
-    for (let i = 0; i < Object.values(document.querySelector("#app > div > div"))[1].children[1]._owner.stateNode.enemies.length; i++) {
-        Object.values(document.querySelector("#app > div > div"))[1].children[1]._owner.stateNode.enemies[i].kill();
+    for (let i = 0; i < Object.values(document.querySelector("#aeffdabedffdb > div > div"))[1].children[1]._owner.stateNode.enemies.length; i++) {
+        Object.values(document.querySelector("#aeffdabedffdb > div > div"))[1].children[1]._owner.stateNode.enemies[i].kill();
     }
 }
 setInterval(killAll, 1);
