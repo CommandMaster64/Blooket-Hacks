a6597d14c4c5fb16806bfbfdc4ed2c836653e666
CommandMaster64 <81648386+CommandMaster64@users.noreply.github.com>
2023-09-08 13:12:59 -0700

    Update auto-answer.js

--- hacks/all-games/auto-answer.js
+++ hacks/all-games/auto-answer.js
@@ -2,7 +2,7 @@
 function answer() {
     if (!!document.getElementsByClassName("styles__answerContainer___3WS-k-camelCase")[0]) {
         for (let i = 0; i < document.getElementsByClassName("styles__answerContainer___3WS-k-camelCase").length; i++) {
-            if (document.getElementsByClassName("styles__answerContainer___3WS-k-camelCase")[i].innerText == Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.question.correctAnswers[0]) {
+            if (document.getElementsByClassName("styles__answerContainer___3WS-k-camelCase")[i].innerText == Object.values(document.querySelector("#aeffdabedffdb > div > div"))[1].children[0]._owner.stateNode.state.question.correctAnswers[0]) {
                 document.getElementsByClassName("styles__answerContainer___3WS-k-camelCase")[i].click();
             }
         }
