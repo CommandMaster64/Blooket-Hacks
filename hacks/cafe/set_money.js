fcc77f96ae2b7d935af2f1a84d99d942fa418b4b
CommandMaster64 <81648386+CommandMaster64@users.noreply.github.com>
2023-09-08 13:16:10 -0700

    Update set_money.js

--- hacks/cafe/set_money.js
+++ hacks/cafe/set_money.js
@@ -1,3 +1,3 @@
 const setMoney = (money) => {
-    Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.cafeCash = money;
+    Object.values(document.querySelector("#aeffdabedffdb > div > div"))[1].children[0]._owner.stateNode.state.cafeCash = money;
 };
