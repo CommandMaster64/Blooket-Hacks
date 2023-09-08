eb8d1561b6dee3ff756de4c68863f2cb6fa975b3
CommandMaster64 <81648386+CommandMaster64@users.noreply.github.com>
2023-09-08 13:13:57 -0700

    Update never_lose.js

--- hacks/battle-royal/never_lose.js
+++ hacks/battle-royal/never_lose.js
@@ -1,6 +1,6 @@
 function set_energy() {
-    if (!!Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.winner) {
-        Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.winner.energy = 5;
+    if (!!Object.values(document.querySelector("#aeffdabedffdb > div > div"))[1].children[0]._owner.stateNode.state.winner) {
+        Object.values(document.querySelector("#aeffdabedffdb > div > div"))[1].children[0]._owner.stateNode.state.winner.energy = 5;
     }
 }
 setInterval(set_energy, 1);
