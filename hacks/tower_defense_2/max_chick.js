727c465eac2de91ae265573b76f3b3e3122a7432
CommandMaster64 <81648386+CommandMaster64@users.noreply.github.com>
2023-09-08 13:19:09 -0700

    Update max_chick.js

--- hacks/tower_defense_2/max_chick.js
+++ hacks/tower_defense_2/max_chick.js
@@ -1,4 +1,4 @@
-Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.towers[0].stats.dmg = 9999;
-Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.towers[0].stats.fireRate = 100;
-Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.towers[0].stats.maxTargets = 9999;
-Object.values(document.querySelector("#app > div > div"))[1].children[0]._owner.stateNode.state.towers[0].stats.range = 9999;
+Object.values(document.querySelector("#aeffdabedffdb > div > div"))[1].children[0]._owner.stateNode.state.towers[0].stats.dmg = 9999;
+Object.values(document.querySelector("#aeffdabedffdb > div > div"))[1].children[0]._owner.stateNode.state.towers[0].stats.fireRate = 100;
+Object.values(document.querySelector("#aeffdabedffdb > div > div"))[1].children[0]._owner.stateNode.state.towers[0].stats.maxTargets = 9999;
+Object.values(document.querySelector("#aeffdabedffdb > div > div"))[1].children[0]._owner.stateNode.state.towers[0].stats.range = 9999;
