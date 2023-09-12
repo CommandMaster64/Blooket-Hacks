d9481b2fcd3cbda1be50700d0fdb5b2b2b69d25d
CommandMaster64 <81648386+CommandMaster64@users.noreply.github.com>
2023-09-12 15:07:32 -0700

    Update infinite_food.js

--- hacks/cafe/infinite_food.js
+++ hacks/cafe/infinite_food.js
@@ -2,9 +2,9 @@ const obj = (() => {
     return Object.values(document.getElementsByClassName("arts__body___3acI_-camelCase")[0])[1].children[0]._owner.stateNode;
 })();
 function restock() {
-    if (!!obj.children[0]._owner.stateNode.state.foods) {
-        for (let i = 0; i < obj.children[0]._owner.stateNode.state.foods.length; i++) {
-            obj.children[0]._owner.stateNode.state.foods[i].stock = 99;
+    if (!!obj.state.foods) {
+        for (let i = 0; i < obj.state.foods.length; i++) {
+            obj.state.foods[i].stock = 99;
         }
     }
 }
