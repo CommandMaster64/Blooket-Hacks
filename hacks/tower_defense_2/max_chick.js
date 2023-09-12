57d2f748e8f49127205fc44de5503337a45a2a66
CommandMaster64 <81648386+CommandMaster64@users.noreply.github.com>
2023-09-12 15:32:06 -0700

    Update max_chick.js

--- hacks/tower_defense_2/max_chick.js
+++ hacks/tower_defense_2/max_chick.js
@@ -1,7 +1,7 @@
-const obj = (() => {
+const obj = () => {
     return Object.values(document.getElementsByClassName("arts__body___3acI_-camelCase")[0])[1].children[0]._owner.stateNode;
-})();
-obj.state.towers[0].stats.dmg = 9999;
-obj.state.towers[0].stats.fireRate = 100;
-obj.state.towers[0].stats.maxTargets = 9999;
-obj.state.towers[0].stats.range = 9999;
+};
+obj().state.towers[0].stats.dmg = 9999;
+obj().state.towers[0].stats.fireRate = 100;
+obj().state.towers[0].stats.maxTargets = 9999;
+obj().state.towers[0].stats.range = 9999;
