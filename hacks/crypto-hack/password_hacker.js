d2872c5cb2e9bf748dd36bc876d4ffa2608a4958
CommandMaster64 <81648386+CommandMaster64@users.noreply.github.com>
2023-09-12 15:09:26 -0700

    Update password_hacker.js

--- hacks/crypto-hack/password_hacker.js
+++ hacks/crypto-hack/password_hacker.js
@@ -1,5 +1,8 @@
+const obj = (() => {
+    return Object.values(document.getElementsByClassName("arts__body___3acI_-camelCase")[0])[1].children[0]._owner.stateNode;
+})();
 for (let i = 0; i < document.getElementsByClassName("styles__button___2OOoS-camelCase").length; i++) {
-    if (document.getElementsByClassName("styles__button___2OOoS-camelCase")[i].innerText == Object.values(document.querySelector("#aeffdabedffdb > div > div"))[1].children[0]._owner.stateNode.state.correctPassword) {
+    if (document.getElementsByClassName("styles__button___2OOoS-camelCase")[i].innerText == obj.state.correctPassword) {
         document.getElementsByClassName("styles__button___2OOoS-camelCase")[i].click();
     }
 }
