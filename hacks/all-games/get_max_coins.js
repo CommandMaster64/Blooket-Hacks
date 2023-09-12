8fc7c63d08412e27f8c0cb173466b5fa3484e2b4
CommandMaster64 <81648386+CommandMaster64@users.noreply.github.com>
2023-09-12 14:53:34 -0700

    Update get_max_coins.js

--- hacks/all-games/get_max_coins.js
+++ hacks/all-games/get_max_coins.js
@@ -1,3 +1,6 @@
+const obj = (() => {
+    return Object.values(document.getElementsByClassName("arts__body___3acI_-camelCase")[0])[1].children[0]._owner.stateNode;
+})();
 for (let i = 0; i < 5000; i++) {
     Object.values(document.querySelector("#aeffdabedffdb > div > div"))[1].children[0]._owner.stateNode.corrects[i] = 1;
 }
