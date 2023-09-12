a30e833cc40bb2162d98d6e6e29fbefc9b79ed96
CommandMaster64 <81648386+CommandMaster64@users.noreply.github.com>
2023-09-12 15:31:55 -0700

    Update always_win.js

--- hacks/tower-of-doom/always_win.js
+++ hacks/tower-of-doom/always_win.js
@@ -1,14 +1,14 @@
-const obj = (() => {
+const obj = () => {
     return Object.values(document.getElementsByClassName("arts__body___3acI_-camelCase")[0])[1].children[0]._owner.stateNode;
-})();
+};
 function set() {
-    if (!!obj.state.enemyCard) {
-        obj.state.enemyCard.strength = 0;
-        obj.state.enemyCard.charisma = 0;
-        obj.state.enemyCard.wisdom = 0;
-        obj.state.myCard.strength = 999;
-        obj.state.myCard.charisma = 999;
-        obj.state.myCard.wisdom = 999;
+    if (!!obj().state.enemyCard) {
+        obj().state.enemyCard.strength = 0;
+        obj().state.enemyCard.charisma = 0;
+        obj().state.enemyCard.wisdom = 0;
+        obj().state.myCard.strength = 999;
+        obj().state.myCard.charisma = 999;
+        obj().state.myCard.wisdom = 999;
     }
 }
 setInterval(set, 1);
