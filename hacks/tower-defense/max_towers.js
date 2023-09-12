b998e7166f39bf8bbc5b0b2f2bb6bb754158824c
CommandMaster64 <81648386+CommandMaster64@users.noreply.github.com>
2023-09-12 15:31:37 -0700

    Update max_towers.js

--- hacks/tower-defense/max_towers.js
+++ hacks/tower-defense/max_towers.js
@@ -1,9 +1,9 @@
-const obj = (() => {
+const obj = () => {
     return Object.values(document.getElementsByClassName("arts__body___3acI_-camelCase")[0])[1].children[0]._owner.stateNode;
-})();
-for (let i = 0; i < obj.towers.length; i++) {
-    obj.towers[i].range = 999;
-    obj.towers[i].damage = 999;
-    obj.towers[i].fullCd = 0;
-    obj.p5._targetFrameRate = 999;
+};
+for (let i = 0; i < obj().towers.length; i++) {
+    obj().towers[i].range = 999;
+    obj().towers[i].damage = 999;
+    obj().towers[i].fullCd = 0;
+    obj().p5._targetFrameRate = 999;
 }
