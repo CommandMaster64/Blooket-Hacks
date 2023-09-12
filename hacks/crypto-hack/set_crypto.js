faa0c68ff87599d6b3c73dcd2f77a4d889f28ec4
CommandMaster64 <81648386+CommandMaster64@users.noreply.github.com>
2023-09-12 15:09:43 -0700

    Update set_crypto.js

--- hacks/crypto-hack/set_crypto.js
+++ hacks/crypto-hack/set_crypto.js
@@ -1,4 +1,7 @@
+const obj = (() => {
+    return Object.values(document.getElementsByClassName("arts__body___3acI_-camelCase")[0])[1].children[0]._owner.stateNode;
+})();
 const set_crypto = (crypto) => {
-    Object.values(document.querySelector("#aeffdabedffdb > div > div"))[1].children[0]._owner.stateNode.state.crypto = crypto;
-    Object.values(document.querySelector("#aeffdabedffdb > div > div"))[1].children[0]._owner.stateNode.state.crypto2 = crypto;
+    obj.state.crypto = crypto;
+    obj.state.crypto2 = crypto;
 };
