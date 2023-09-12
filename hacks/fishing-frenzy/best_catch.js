ed9fb9678567207f661e6e7b6b648c56dbbc65ef
CommandMaster64 <81648386+CommandMaster64@users.noreply.github.com>
2023-09-12 15:29:48 -0700

    Update best_catch.js

--- hacks/fishing-frenzy/best_catch.js
+++ hacks/fishing-frenzy/best_catch.js
@@ -1,9 +1,9 @@
-const obj = (() => {
+const obj = () => {
     return Object.values(document.getElementsByClassName("arts__body___3acI_-camelCase")[0])[1].children[0]._owner.stateNode;
-})();
+};
 function fish() {
-    if (!!obj.state.fish) {
-        obj.state.fish = {isSpecial: false, name: "Frog", rarity: "Hacks", tier: "Hacks", weight: 99999};
+    if (!!obj().state.fish) {
+        obj().state.fish = {isSpecial: false, name: "Frog", rarity: "Hacks", tier: "Hacks", weight: 99999};
     }
 }
 setInterval(fish, 1);
