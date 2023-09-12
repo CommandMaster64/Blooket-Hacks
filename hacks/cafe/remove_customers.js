f42a7eda7fec65ba70b21c13878eeded640835b4
CommandMaster64 <81648386+CommandMaster64@users.noreply.github.com>
2023-09-12 15:25:36 -0700

    Update remove_customers.js

--- hacks/cafe/remove_customers.js
+++ hacks/cafe/remove_customers.js
@@ -2,11 +2,11 @@ const obj = () => {
     return Object.values(document.getElementsByClassName("arts__body___3acI_-camelCase")[0])[1].children[0]._owner.stateNode;
 };
 function removeCustomers() {
-    if (!!obj.state.customers) {
-        for (let i = 0; i < obj.state.customers.length; i++) {
-            if (!!obj.state.customers[i].order && !obj.state.customers[i].w) {
-                obj.removeCustomer(i, 1);
-                obj.state.customers[i].w = 1;
+    if (!!obj().state.customers) {
+        for (let i = 0; i < obj().state.customers.length; i++) {
+            if (!!obj().state.customers[i].order && !obj().state.customers[i].w) {
+                obj().removeCustomer(i, 1);
+                obj().state.customers[i].w = 1;
             }
         }
     }
