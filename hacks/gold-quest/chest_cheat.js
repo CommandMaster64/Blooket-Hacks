9b7d4e3fe59f498be79f6680c4a144d5a8827f3e
CommandMaster64 <81648386+CommandMaster64@users.noreply.github.com>
2023-09-12 15:11:19 -0700

    Update chest_cheat.js

--- hacks/gold-quest/chest_cheat.js
+++ hacks/gold-quest/chest_cheat.js
@@ -1,13 +1,16 @@
+const obj = (() => {
+    return Object.values(document.getElementsByClassName("arts__body___3acI_-camelCase")[0])[1].children[0]._owner.stateNode;
+})();
 function set_chest_cheat() {
     if (!!document.getElementsByClassName("styles__choice1___27uim-camelCase")[0]) {
-        if (!document.getElementsByClassName("styles__choice1___27uim-camelCase")[0].innerHTML.includes(Object.values(document.querySelector("#aeffdabedffdb > div > div"))[1].children[0]._owner.stateNode.state.choices[0].text)) {
-            document.getElementsByClassName("styles__choice1___27uim-camelCase")[0].innerHTML += Object.values(document.querySelector("#aeffdabedffdb > div > div"))[1].children[0]._owner.stateNode.state.choices[0].text;
+        if (!document.getElementsByClassName("styles__choice1___27uim-camelCase")[0].innerHTML.includes(obj.state.choices[0].text)) {
+            document.getElementsByClassName("styles__choice1___27uim-camelCase")[0].innerHTML += obj.state.choices[0].text;
         }
-        if (!document.getElementsByClassName("styles__choice2___1aP2D-camelCase")[0].innerHTML.includes(Object.values(document.querySelector("#aeffdabedffdb > div > div"))[1].children[0]._owner.stateNode.state.choices[1].text)) {
-            document.getElementsByClassName("styles__choice2___1aP2D-camelCase")[0].innerHTML += Object.values(document.querySelector("#aeffdabedffdb > div > div"))[1].children[0]._owner.stateNode.state.choices[1].text;
+        if (!document.getElementsByClassName("styles__choice2___1aP2D-camelCase")[0].innerHTML.includes(obj.state.choices[1].text)) {
+            document.getElementsByClassName("styles__choice2___1aP2D-camelCase")[0].innerHTML += obj.state.choices[1].text;
         }
-        if (!document.getElementsByClassName("styles__choice3___2L6Q--camelCase")[0].innerHTML.includes(Object.values(document.querySelector("#aeffdabedffdb > div > div"))[1].children[0]._owner.stateNode.state.choices[2].text)) {
-            document.getElementsByClassName("styles__choice3___2L6Q--camelCase")[0].innerHTML += Object.values(document.querySelector("#aeffdabedffdb > div > div"))[1].children[0]._owner.stateNode.state.choices[2].text;
+        if (!document.getElementsByClassName("styles__choice3___2L6Q--camelCase")[0].innerHTML.includes(obj.state.choices[2].text)) {
+            document.getElementsByClassName("styles__choice3___2L6Q--camelCase")[0].innerHTML += obj.state.choices[2].text;
         }
     }
 }
