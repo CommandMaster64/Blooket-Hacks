1e5b26fe74baef791ac56133d3dafc3801d6251d
CommandMaster64 <81648386+CommandMaster64@users.noreply.github.com>
2023-09-12 15:30:16 -0700

    Update no_gold_swap.js

--- hacks/gold-quest/no_gold_swap.js
+++ hacks/gold-quest/no_gold_swap.js
@@ -1,7 +1,7 @@
-const obj = (() => {
+const obj = () => {
     return Object.values(document.getElementsByClassName("arts__body___3acI_-camelCase")[0])[1].children[0]._owner.stateNode;
-})();
-obj.state.gold = "b";
-obj.state.gold2 = "b";
-obj.state.choices[0].type = "swap";
-obj.state.choices[0].text = "swap";
+};
+obj().state.gold = "b";
+obj().state.gold2 = "b";
+obj().state.choices[0].type = "swap";
+obj().state.choices[0].text = "swap";
