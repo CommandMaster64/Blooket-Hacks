6a3919ee28a36b01d2ea0a6eaf6b1cf5f2a6b917
CommandMaster64 <81648386+CommandMaster64@users.noreply.github.com>
2023-09-12 15:26:22 -0700

    Update never_lose.js

--- hacks/crazy-kingdom/never_lose.js
+++ hacks/crazy-kingdom/never_lose.js
@@ -1,10 +1,10 @@
-const obj = (() => {
+const obj = () => {
     return Object.values(document.getElementsByClassName("arts__body___3acI_-camelCase")[0])[1].children[0]._owner.stateNode;
-})();
+};
 function setThings() {
-    obj.state.happiness = 999;
-    obj.state.materials = 999;
-    obj.state.people = 999;
-    obj.state.gold = 999;
+    obj().state.happiness = 999;
+    obj().state.materials = 999;
+    obj().state.people = 999;
+    obj().state.gold = 999;
 }
 setInterval(setThings, 1);
