32385aab4e0106d9787bf0acda231289c5322882
CommandMaster64 <81648386+CommandMaster64@users.noreply.github.com>
2023-09-12 15:29:21 -0700

    Update max_blooks.js

--- hacks/factory/max_blooks.js
+++ hacks/factory/max_blooks.js
@@ -1,13 +1,13 @@
-const obj = (() => {
+const obj = () => {
     return Object.values(document.getElementsByClassName("arts__body___3acI_-camelCase")[0])[1].children[0]._owner.stateNode;
-})();
-for (let i = 0; i < obj.state.choices.length; i++) {
-    obj.state.choices[i].cash = [999999];
-    obj.state.choices[i].time = [0.01];
-    obj.state.choices[i].price = [];
+};
+for (let i = 0; i < obj().state.choices.length; i++) {
+    obj().state.choices[i].cash = [999999];
+    obj().state.choices[i].time = [0.01];
+    obj().state.choices[i].price = [];
 }
-for (let i = 0; i < obj.state.blooks.length; i++) {
-    obj.state.blooks[i].cash = [999999];
-    obj.state.blooks[i].time = [0.01];
-    obj.state.blooks[i].price = [];
+for (let i = 0; i < obj().state.blooks.length; i++) {
+    obj().state.blooks[i].cash = [999999];
+    obj().state.blooks[i].time = [0.01];
+    obj().state.blooks[i].price = [];
 }
