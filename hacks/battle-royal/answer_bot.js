e783a26d7465c9f14b56eadf670c5b4dfd2ae1b4
CommandMaster64 <81648386+CommandMaster64@users.noreply.github.com>
2023-09-12 15:06:03 -0700

    Update answer_bot.js

--- hacks/battle-royal/answer_bot.js
+++ hacks/battle-royal/answer_bot.js
@@ -1,7 +1,10 @@
+const obj = (() => {
+    return Object.values(document.getElementsByClassName("arts__body___3acI_-camelCase")[0])[1].children[0]._owner.stateNode;
+})();
 function answer() {
     if (!!document.getElementsByClassName("styles__answerContainer___3WS-k-camelCase")[0]) {
         for (let i = 0; i < document.getElementsByClassName("styles__answerContainer___3WS-k-camelCase").length; i++) {
-            if (document.getElementsByClassName("styles__answerContainer___3WS-k-camelCase")[i].innerText == Object.values(document.querySelector("#aeffdabedffdb > div > div"))[1].children[0]._owner.stateNode.props.client.question.correctAnswers[0]) {
+            if (document.getElementsByClassName("styles__answerContainer___3WS-k-camelCase")[i].innerText == obj.props.client.question.correctAnswers[0]) {
                 document.getElementsByClassName("styles__answerContainer___3WS-k-camelCase")[i].click();
             }
         }
