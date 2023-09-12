04fb24032593fb0426e0384a79ab0ee9cae5b54d
CommandMaster64 <81648386+CommandMaster64@users.noreply.github.com>
2023-09-12 15:30:57 -0700

    Update set_xp.js

--- hacks/monster-brawl/set_xp.js
+++ hacks/monster-brawl/set_xp.js
@@ -1,6 +1,6 @@
-const obj = (() => {
+const obj = () => {
     return Object.values(document.getElementsByClassName("arts__body___3acI_-camelCase")[0])[1].children[0]._owner.stateNode;
-})();
+};
 const setXP = (xp) => {
     obj.state.xp = xp;
     obj.state.totalXp = xp;
