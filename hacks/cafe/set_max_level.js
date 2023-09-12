a87a38d2c8070b43a7b5725aae36926106b30bd6
CommandMaster64 <81648386+CommandMaster64@users.noreply.github.com>
2023-09-12 15:25:59 -0700

    Update set_max_level.js

--- hacks/cafe/set_max_level.js
+++ hacks/cafe/set_max_level.js
@@ -1,6 +1,6 @@
 const obj = () => {
     return Object.values(document.getElementsByClassName("arts__body___3acI_-camelCase")[0])[1].children[0]._owner.stateNode;
 };
-for (let i = 0; i < Object.keys(obj.state.items).length; i++) {
-    obj.state.items[Object.keys(obj.state.items)[i]] = 5;
+for (let i = 0; i < Object.keys(obj().state.items).length; i++) {
+    obj().state.items[Object.keys(obj().state.items)[i]] = 5;
 }
