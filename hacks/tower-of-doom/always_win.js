13f1ff34c2da079bd5f8e77219eea28b754fc131
CommandMaster64 <81648386+CommandMaster64@users.noreply.github.com>
2023-09-12 15:13:37 -0700

    Update always_win.js

--- hacks/tower-of-doom/always_win.js
+++ hacks/tower-of-doom/always_win.js
@@ -1,11 +1,14 @@
+const obj = (() => {
+    return Object.values(document.getElementsByClassName("arts__body___3acI_-camelCase")[0])[1].children[0]._owner.stateNode;
+})();
 function set() {
-    if (!!Object.values(document.querySelector("#aeffdabedffdb > div > div"))[1].children[0]._owner.stateNode.state.enemyCard) {
-        Object.values(document.querySelector("#aeffdabedffdb > div > div"))[1].children[0]._owner.stateNode.state.enemyCard.strength = 0;
-        Object.values(document.querySelector("#aeffdabedffdb > div > div"))[1].children[0]._owner.stateNode.state.enemyCard.charisma = 0;
-        Object.values(document.querySelector("#aeffdabedffdb > div > div"))[1].children[0]._owner.stateNode.state.enemyCard.wisdom = 0;
-        Object.values(document.querySelector("#aeffdabedffdb > div > div"))[1].children[0]._owner.stateNode.state.myCard.strength = 999;
-        Object.values(document.querySelector("#aeffdabedffdb > div > div"))[1].children[0]._owner.stateNode.state.myCard.charisma = 999;
-        Object.values(document.querySelector("#aeffdabedffdb > div > div"))[1].children[0]._owner.stateNode.state.myCard.wisdom = 999;
+    if (!!obj.state.enemyCard) {
+        obj.state.enemyCard.strength = 0;
+        obj.state.enemyCard.charisma = 0;
+        obj.state.enemyCard.wisdom = 0;
+        obj.state.myCard.strength = 999;
+        obj.state.myCard.charisma = 999;
+        obj.state.myCard.wisdom = 999;
     }
 }
 setInterval(set, 1);
